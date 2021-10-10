b9249b583f2e2855b6cef72b4d5c419a264cfca1
seyed-mostafa <mostafa.atyabi.1380@gmail.com>
2021-10-10 16:03:38 +0330

    canny pic

--- line.py
+++ line.py
@@ -3,8 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 
-
 image = cv2.imread('images/test_image.jpg')
-image = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
-plt.imshow(image)
-plt.show()
+blur = cv2.GaussianBlur(cv2.cvtColor(image,cv2.COLOR_BGR2GRAY),(5,5),0)
+canny = cv2.Canny(blur,50,150)
+cv2.imshow("canny picture",canny)
+cv2.waitKey()
+
