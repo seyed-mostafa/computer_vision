86fcd9696d3c9f721a7771a771b7536546f5c7de
seyed-mostafa <mostafa.atyabi.1380@gmail.com>
2021-10-11 21:53:19 +0330

    average of left and right lines

--- line.py
+++ line.py
@@ -5,7 +5,7 @@ import imutils
 
 def input_gray_image(path):
     image = cv2.imread(path)
-    return image
+    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 def canny(image):
     blur = cv2.GaussianBlur(image, (5, 5), 0)
@@ -24,15 +24,41 @@ def display_line(image,lines):
     if lines is not None:
         for line in lines:
             x1, y1, x2, y2 = line.reshape(4)
-            cv2.line(line_image,(x1,y1),(x2,y2),(255,0,0),5)
+            cv2.line(line_image,(x1,y1),(x2,y2),(255,0,0),10)
     return line_image
+
+def make_coordinates(image,line_parameters):
+    slope,intercept = line_parameters
+    y1 = image.shape[0]
+    y2 = int(y1*3/5)
+    x1 = int((y1 - intercept)/slope)
+    x2 = int((y2 - intercept)/slope)
+    return np.array([x1,y1,x2,y2])
+
+def average_slope_intercept(image,lines):
+    left_fit, right_fit = [],[]
+    for line in lines:
+        x1, y1, x2, y2 = line.reshape(4)
+        parameters = np.polyfit((x1,x2),(y1,y2),1)
+        slope = parameters[0]
+        intercept = parameters[1]
+        if slope < 0:
+            left_fit.append((slope,intercept))
+        else:
+            right_fit.append((slope,intercept))
+    left_fit_average = np.average(left_fit, axis=0)
+    right_fit_average = np.average(right_fit, axis=0)
+    right_line = make_coordinates(image,right_fit_average)
+    left_line = make_coordinates(image,left_fit_average)
+    return np.array([left_line,right_line])
 
 
 src = input_gray_image("images/test_image.jpg")
+image = cv2.imread("images/test_image.jpg")
 lines = cv2.HoughLinesP(region(canny(src)), 2, np.pi/180,100,np.array([]),40,5)
-
-line_image = display_line(src,lines)
-combo_image = cv2.addWeighted(line_image,0.8,src,1,1)
+averaged_lines = average_slope_intercept(src, lines)
+line_image = display_line(image,averaged_lines)
+combo_image = cv2.addWeighted(line_image,1,image,0.8,1)
 
 
 cv2.imshow('combo image',combo_image)
