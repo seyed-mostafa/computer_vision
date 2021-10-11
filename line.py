108eee702605926b1b5d7a1b77cd8e3f1bd0d463
seyed-mostafa <mostafa.atyabi.1380@gmail.com>
2021-10-11 17:47:14 +0330

    show lines on the rgb image

--- line.py
+++ line.py
@@ -5,12 +5,12 @@ import imutils
 
 def input_gray_image(path):
     image = cv2.imread(path)
-    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    return image
 
 def canny(image):
     blur = cv2.GaussianBlur(image, (5, 5), 0)
     canny = cv2.Canny(blur, 50, 150)
-    return canny
+    return imutils.auto_canny(blur, sigma=0.33)
 
 def region(image):
    height = image.shape[0]
@@ -19,14 +19,21 @@ def region(image):
    cv2.fillPoly(mask,triangle,255)
    return cv2.bitwise_and(image,mask)
 
-def display_line(image):
-    return 2
+def display_line(image,lines):
+    line_image = np.zeros_like(image)
+    if lines is not None:
+        for line in lines:
+            x1, y1, x2, y2 = line.reshape(4)
+            cv2.line(line_image,(x1,y1),(x2,y2),(255,0,0),5)
+    return line_image
 
 
 src = input_gray_image("images/test_image.jpg")
-#lines = cv2.HoughLinesP()
-cv2.imshow("region",imutils.auto_canny(src,sigma=0.33))
-cv2.waitKey()
-cv2.imshow("region",region(canny(src)))
-cv2.waitKey()
+lines = cv2.HoughLinesP(region(canny(src)), 2, np.pi/180,100,np.array([]),40,5)
 
+line_image = display_line(src,lines)
+combo_image = cv2.addWeighted(line_image,0.8,src,1,1)
+
+
+cv2.imshow('combo image',combo_image)
+cv2.waitKey(0)
