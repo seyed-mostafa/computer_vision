5042a20af411deff792eb4b27226980532b2fa2a
seyed-mostafa <mostafa.atyabi.1380@gmail.com>
2021-10-11 22:05:52 +0330

    add video to process

--- line.py
+++ line.py
@@ -22,8 +22,7 @@ def region(image):
 def display_line(image,lines):
     line_image = np.zeros_like(image)
     if lines is not None:
-        for line in lines:
-            x1, y1, x2, y2 = line.reshape(4)
+        for x1, y1, x2, y2 in lines:
             cv2.line(line_image,(x1,y1),(x2,y2),(255,0,0),10)
     return line_image
 
