1887e8c5db063ae68081ef71b771dcd614c8db1b
seyed-mostafa <mostafa.atyabi.1380@gmail.com>
2021-10-23 05:31:41 +0330

    create canny img

--- test.py
+++ test.py
@@ -1,10 +1,16 @@
 from turtle import width
 
 import cv2  # computer vision library
+import imutils
 
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+
+def canny(image):
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray_image, (5, 5), 0)
+    return imutils.auto_canny(blur, sigma=0.33)  # better way to canny images
 
 
 def process_frame(image):
@@ -22,7 +28,7 @@ def process_frame(image):
     matrix = cv2.getPerspectiveTransform(pts1, pts2)
     imgOut = cv2.warpPerspective(cropped_img, matrix, (width, height))
 
-    return imgOut
+    return canny(imgOut)
 
 
 video = cv2.VideoCapture('videos/project_video.mp4')
@@ -32,7 +38,7 @@ while video.isOpened():
     if frame is not None:
         processed_frame = process_frame(frame)
         cv2.imshow('video', processed_frame)
-        if cv2.waitKey(20) == 27:
+        if cv2.waitKey(45) == 27:
             break
     else:
         break
